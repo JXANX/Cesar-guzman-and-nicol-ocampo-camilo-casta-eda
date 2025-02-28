07f6681d4e3afda12bee059277b25f2dc7f58bd1
Cesar Andres Guzman <cesarandresguzman27@gmail.com>
2025-02-28 11:56:45 -0500

    Funcionalidad D: Programador A

--- Inventario.java
+++ Inventario.java
@@ -4,4 +4,16 @@ public class Inventario {
     public void metodoA() {
         System.out.println("Ejecutando método A");
     }
+	 public void metodoB() {
+        System.out.println("Ejecutando método B");
+    }
+    public void metodoC() {
+        System.out.println("Ejecutando método C");
+    }
+    
+     
+    public void metodoD() {
+        System.out.println("Ejecutando método D");
+    }
+
 }
